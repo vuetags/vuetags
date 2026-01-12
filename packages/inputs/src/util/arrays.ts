bdd81974193c075cd5d198a5ce2c39d16287f9d2
Michael Verschoof <michael.verschoof@first8.nl>
2026-01-12 10:22:37 +0100

    Forgot the MaybeArray type in the util function

--- packages/inputs/src/util/arrays.ts
+++ packages/inputs/src/util/arrays.ts
@@ -1,9 +1,11 @@
+import { MaybeArray } from '@/components/types';
+
 /**
  * Convert a value to an array, unless it's already an array.
  *
  * @param value the value to convert.
  * @returns an array of the provided value type.
  */
-export function toArray<T>(value: T | T[]): T[] {
+export function toArray<T>(value: MaybeArray<T>): T[] {
     return Array.isArray(value) ? value : [value];
 }
