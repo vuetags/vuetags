3f22dff3326103d0451e4c99377810520269ff96
Michael Verschoof <michael.verschoof@gmail.com>
2026-04-08 10:32:49 +0200

    Created a simple playground base

---
+++ apps/playground/src/components/playground.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import heroImg from '../assets/hero.png';
+import viteLogo from '../assets/vite.svg';
+import vueLogo from '../assets/vue.svg';
+</script>
+
+<template>
+    <header class="title">
+        <div class="hero">
+            <img :src="heroImg" class="base" width="170" height="179" alt="" />
+            <img :src="vueLogo" class="framework" alt="Vue logo" />
+            <img :src="viteLogo" class="vite" alt="Vite logo" />
+        </div>
+
+        <h1>Playground</h1>
+        <p>Testing package implementations and other experiments.</p>
+    </header>
+
+    <main></main>
+</template>
---
+++ apps/playground/vite.config.ts
@@ -0,0 +1,8 @@
+import vue from '@vitejs/plugin-vue';
+// import { defineConfig } from 'vite';
+import { defineConfig } from 'vite-plus';
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [vue()]
+});
