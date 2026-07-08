dc3226c59f4a61ba658fc76ae6a51b0f0efcc473
Michael Verschoof <michael.verschoof@gmail.com>
2026-07-08 12:19:02 +0200

    Updated dependencies and simplified configurations

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -1,14 +1,7 @@
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
 import { defineConfig } from 'vite-plus';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@vuetags/inputs': path.resolve(__dirname, '../../packages/inputs/src'),
-            '@': path.resolve(__dirname, '../../packages/inputs/src')
-        }
-    }
+    plugins: [vue()]
 });
--- packages/inputs/vite.config.ts
+++ packages/inputs/vite.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
                         enabled: true,
                         headless: true,
                         screenshotFailures: false,
-                        instances: [{ browser: 'firefox' }]
+                        instances: [{ browser: 'chromium' }]
                     }
                 }
             }
